cec51db3925f6f2e9aaea5b7dff44ad1fda8f352
Bryan <b.henryon@outlook.fr>
2020-12-09 22:30:58 +0100

    Ajout de WebSocket + communication avec la palette et le canvas

--- client/src/components/Canvas.vue
+++ client/src/components/Canvas.vue
@@ -13,11 +13,14 @@
 
 <script>
 import { mapGetters } from "vuex";
+import io from "socket.io-client";
+
+const socket = io();
 
 export default {
   data() {
     return {
-      drawing: false
+      drawing: false,
     };
   },
   computed: {
@@ -26,13 +29,21 @@ export default {
       "getLineWidth",
       "getCursor",
       "getPaintbrush",
-      "getEraser"
-    ])
+      "getEraser",
+    ]),
   },
   methods: {
     startDrawing(e) {
+      const canvas = document.querySelector(".canvas");
       this.drawing = true;
       this.draw(e);
+
+      socket.emit("startDrawing", {
+        mouseX: e.clientX - canvas.offsetLeft,
+        mouseY: e.clientY - canvas.offsetTop,
+        color: this.getEraser ? "#fff" : this.getColor,
+        lineWidth: this.getLineWidth,
+      });
     },
     stopDrawing() {
       window.addEventListener("mouseup", () => {
@@ -41,11 +52,15 @@ export default {
         const ctx = canvas.getContext("2d");
         ctx.beginPath();
       });
+
+      socket.emit("stopDrawing");
     },
     resetPath() {
       const canvas = document.querySelector(".canvas");
       const ctx = canvas.getContext("2d");
       ctx.beginPath();
+
+      socket.emit("resetPath");
     },
 
     draw(e) {
@@ -71,14 +86,73 @@ export default {
       ctx.stroke();
       ctx.beginPath();
       ctx.moveTo(mouseX, mouseY);
-    }
+
+      socket.emit("draw", {
+        mouseX: e.clientX - canvas.offsetLeft,
+        mouseY: e.clientY - canvas.offsetTop,
+        color: this.getEraser ? "#fff" : this.getColor,
+        lineWidth: this.getLineWidth,
+      });
+    },
   },
   mounted() {
     const canvas = document.querySelector(".canvas");
     const parentCanvas = document.querySelector(".canvas-container");
     canvas.width = parentCanvas.offsetWidth;
     canvas.height = parentCanvas.offsetHeight;
-  }
+
+    socket.on("startDrawing", function(payload) {
+      const canvas = document.querySelector(".canvas");
+      const ctx = canvas.getContext("2d");
+
+      const mouseX = payload.mouseX;
+      const mouseY = payload.mouseY;
+
+      (ctx.lineWidth = payload.lineWidth), (ctx.lineCap = "round");
+
+      (ctx.strokeStyle = payload.color), (canvas.style.cursor = payload.cursor);
+
+      ctx.lineTo(mouseX, mouseY);
+      ctx.stroke();
+      ctx.beginPath();
+      ctx.moveTo(mouseX, mouseY);
+    });
+
+    socket.on("draw", function(payload) {
+      const canvas = document.querySelector(".canvas");
+      const ctx = canvas.getContext("2d");
+
+      const mouseX = payload.mouseX;
+      const mouseY = payload.mouseY;
+
+      (ctx.lineWidth = payload.lineWidth), (ctx.lineCap = "round");
+
+      ctx.strokeStyle = payload.color;
+
+      ctx.lineTo(mouseX, mouseY);
+      ctx.stroke();
+      ctx.beginPath();
+      ctx.moveTo(mouseX, mouseY);
+    });
+
+    socket.on("stopDrawing", () => {
+      const canvas = document.querySelector(".canvas");
+      const ctx = canvas.getContext("2d");
+      ctx.beginPath();
+    });
+
+    socket.on("resetPath", () => {
+      const canvas = document.querySelector(".canvas");
+      const ctx = canvas.getContext("2d");
+      ctx.beginPath();
+    });
+
+    socket.on("clearCanvas", () => {
+      const canvas = document.querySelector(".canvas");
+      const ctx = canvas.getContext("2d");
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+    });
+  },
 };
 </script>
 
--- server.js
+++ server.js
@@ -1,11 +1,35 @@
 const express = require("express");
 const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+io.on('connection', (socket) => { 
+    socket.on("startDrawing", (payload) => {
+        socket.broadcast.emit("startDrawing", payload);
+    })
+
+    socket.on("draw", (payload) => {
+        socket.broadcast.emit("draw", payload);
+    })
+
+    socket.on("stopDrawing", () => {
+        socket.broadcast.emit("stopDrawing");
+    })
+
+    socket.on("resetPath", () => {
+        socket.broadcast.emit("resetPath");
+    });
+
+    socket.on("clearCanvas", () => {
+        socket.broadcast.emit("clearCanvas");
+    })
+});
 
 app.use(express.static(__dirname + '/public/'));
 app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Serveur lancé sur http://localhost:${PORT}`);
 });
